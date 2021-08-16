a7de07b52356551fabc133a9285bea8fe904ceb6
Benjamin Niederee <niederee@gmail.com>
2021-08-16 16:51:09 -0500

    Initial Commit

---
+++ src/OrmBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrmBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
